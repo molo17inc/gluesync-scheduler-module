5ec4f37e9d7d7e1be3f47fb2084930e9d10096f0
Daniele Angeli <daniele.angeli@molo17.com>
2025-04-30 16:06:54 +0800

    format issue fixes

--- gluesync_scheduler/services/scheduler_service.py
+++ gluesync_scheduler/services/scheduler_service.py
@@ -127,7 +127,8 @@ class SchedulerService:
                     elif part in day_map:
                         day_names.append(day_map[part])
                 logger.info(f"Job will run on these days: {day_names}")
-                cron_dow = ",".join(day_names)
+                # APScheduler's CronTrigger expects uppercase day names
+                cron_dow = ",".join([day.upper() for day in day_names])
             else:
                 logger.info("Job will run every day")
                 cron_dow = "*"
