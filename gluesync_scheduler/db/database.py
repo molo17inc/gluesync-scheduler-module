ff47f6ba0e61df459a1077581b24b32ee2dacf7b
Daniele Angeli <daniele.angeli@molo17.com>
2025-08-24 14:46:42 +0200

    improvements

--- gluesync_scheduler/db/database.py
+++ gluesync_scheduler/db/database.py
@@ -22,7 +22,7 @@
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from gluesync_scheduler.config.settings import settings
